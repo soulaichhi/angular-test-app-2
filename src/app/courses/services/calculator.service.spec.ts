c1d35ebcf2801753052e8a75bbe6eb3a3327ff88
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-08-06 18:33:03 +0100

    getting familiar with testing

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,15 +1,30 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => {
+  let calculator: CalculatorService, loggerSpy: any;
+  beforeEach(() => {
+    console.log('calling beforeEach');
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+    calculator = TestBed.get(CalculatorService);
+  });
   it('should add 2 numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    console.log('Add Test');
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract 2 numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    console.log('Subtract Test');
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
