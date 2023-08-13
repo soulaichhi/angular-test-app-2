ecbf69790c9b58353aadbb753ad9ac0edae6e2f8
soulaichhi <chhibou.soulaimane@gmail.com>
2023-08-13 14:52:06 +0100

    Complete e2e Testing Section

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:4200/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -9,9 +10,9 @@ export default defineConfig({
 
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 });
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,30 @@
 describe('Home Page', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/courses',
+      },
+      {
+        statusCode: 200,
+        fixture: 'courses.json',
+      },
+    ).as('courses');
+    cy.visit('/');
+  });
   it('should display a list of courses', () => {
-    cy.visit('http://localhost:4200/');
+    cy.contains('All Courses');
+    cy.wait('@courses');
+    cy.get('mat-card').should('have.length', 9);
+  });
+  it('should display the advanced courses', () => {
+    cy.get('.mdc-tab__text-label').should('have.length', 2);
+    cy.get('.mdc-tab__text-label').last().click();
+    cy.get('.mat-mdc-tab-body-content .mat-mdc-card-title')
+      .its('length')
+      .should('be.gt', 1);
+    cy.get('.mat-mdc-tab-body-content .mat-mdc-card-title')
+      .first()
+      .should('contain', 'Angular Security Course');
   });
 });
